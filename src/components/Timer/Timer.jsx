8bfb55fe907dca763575788de6ceb1136397b127
Maja Thunberg <majaneko@gmail.com>
2022-02-19 13:29:14 +0100

    Add Timer component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,18 +1,57 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
 import styles from './Timer.module.scss'
 
 
-export const Timer = () => {
-  return (
-    <motion.div
-      className={styles.result}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1, delay: 1.4 }} 
-    >
-      Hej
+export const Timer = ({ time = 5, onFinish = () => {}, isStarted = false }) => {
+  const [secondsLeft, setSecondsLeft] = useState(time)
 
-    </motion.div>
+  useEffect(() => {
+    let timer
+    if (isStarted && secondsLeft > 0) {
+      timer = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1)
+      }, 1000)
+    }
+    return () => {
+      timer && clearTimeout(timer)
+    }
+  }, [isStarted, secondsLeft])
+
+  useEffect(() => {
+    if (secondsLeft === 0) {
+      onFinish()
+      return
+    }
+  }, [secondsLeft, onFinish])
+
+  return (
+    <div className={`${styles.wrapper} ${isStarted && styles.started}`}>
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <g className={styles.countDown}>
+          <circle 
+            className={styles.timeElapsed} 
+            cx="50" 
+            cy="50" 
+            r="45"
+            style={{ transition: `${time}s linear all` }}
+          ></circle>
+          <path
+            id="base-timer-path-remaining"
+            className={styles.timeLeft}
+            d="
+              M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0
+            "
+            style={{ transition: `${time}s linear all` }}
+          ></path>
+        </g>
+      </svg>
+      <div className={styles.label}>
+        <p className={styles.description}>Time left:</p>
+        <p className={styles.time}>{secondsLeft}</p>
+      </div>
+    </div>
   )
 }
