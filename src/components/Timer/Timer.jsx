2d91bc3d129b4f99883bc6c624d1c8d07c2860e2
Maja Thunberg <majaneko@gmail.com>
2022-02-22 08:37:48 +0100

    Start animation using useAnimation

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
+import React, { useCallback, useEffect, useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
 import styles from './Timer.module.scss'
 
 
-export const Timer = ({ time = 0, onFinish = () => {}, isRunning = false, hasUpdated = false }) => {
+export const Timer = ({ time = 0, onFinish, isRunning, hasUpdated }) => {
   const [secondsLeft, setSecondsLeft] = useState(time)
+  const animation = useAnimation()
 
   function getTime() {
     if (secondsLeft < 3600) {
@@ -15,6 +16,15 @@ export const Timer = ({ time = 0, onFinish = () => {}, isRunning = false, hasUpd
     return date.toISOString().slice(11, 19)
   }
 
+  const startAnimation = useCallback(async () => {
+      await animation.start({ transform: 'scaleX(-1) rotate(90deg)', transition: { duration: 0 } })
+      await animation.start({ strokeDasharray: '283 283', stroke: '#ffb5b5', transition: { duration: 0.5 } })
+      await animation.start({ transform: 'scaleX(1) rotate(90deg)', transition: { duration: 0 } })
+      await animation.start({ strokeDasharray: '282 283', transition: { duration: time / 900 }, ease: 'easeOut' })
+      animation.start({ strokeDasharray: '0 283', transition: { duration: time - (time / 900), ease: 'linear' } })
+    }
+  , [time, animation]) 
+
   useEffect(() => {
     setSecondsLeft(time)
   }, [time])
@@ -24,6 +34,10 @@ export const Timer = ({ time = 0, onFinish = () => {}, isRunning = false, hasUpd
       setSecondsLeft(time)
     } 
   }, [hasUpdated, time])
+
+  useEffect(() => {
+    isRunning && startAnimation()
+  }, [isRunning, startAnimation])
   
   useEffect(() => {
     let timer
@@ -49,70 +63,58 @@ export const Timer = ({ time = 0, onFinish = () => {}, isRunning = false, hasUpd
       stroke: '#dddddd',
     },
     start: {
-      stroke: '#ffb5b5',
+      stroke: '#dbd0d0',
       transition: { duration: time, ease: 'linear' }
     },
     reset: { 
       stroke: '#dddddd',
-      transition: { duration: 0 }
+      transition: { duration: .8 }
     }
   }
   
   const timeLeftVariants = {
     initial: {
-      strokeDasharray: '283 283',
-      stroke: '#ffb5b5',
-    },
-    start: {
       strokeDasharray: '0 283',
-      stroke: ['#ffb5b5', '#9399b4', '#dddddd',],
-      transition: { duration: time, ease: 'linear' }
     },
-    reset: { 
-      strokeDasharray: '0 283',
-      stroke: '#ffb5b5',
-      transition: { duration: 0.5 }
-    },
-    something: { 
-      strokeDasharray: '283 283',
-      stroke: '#ffb5b5',
-      transition: { duration: 0 }
+    reset: () => {
+      animation.stop()
+      animation.start({ strokeDasharray: '0 283', transition: { duration: 0.8, ease: 'linear' } })
     }
   }
 
   return (
-    <div 
-      className={`${styles.wrapper} ${isRunning && styles.started} ${hasUpdated && styles.restarted}`}
-    >
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <g className={styles.countDown}>
-          <motion.circle 
-            className={styles.timeElapsed} 
-            cx="50" 
-            cy="50" 
-            r="45"
-            variants={elapsedTimeVariants}
-            initial="initial"
-            animate={isRunning ? 'start' : 'initial'}
-          ></motion.circle>
-          <motion.path
-            id="base-timer-path-remaining"
-            className={styles.timeLeft}
-            d="
-              M 50, 50
-              m -45, 0
-              a 45,45 0 1,0 90,0
-              a 45,45 0 1,0 -90,0
-            "
-            variants={timeLeftVariants}
-            initial="initial"
-            animate={isRunning ? 'start' : 'initial'}
-          ></motion.path>
-        </g>
-      </svg>
-      <div className={styles.label}>
-        <p className={styles.description}>Time until next step:</p>
-        <p className={styles.time}>{getTime()}</p>
+    <div className={styles.square}>
+      <div className={`${styles.wrapper} ${isRunning && styles.started} ${hasUpdated && styles.restarted}`}>
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <g className={styles.countDown}>
+            <motion.circle 
+              className={styles.timeElapsed} 
+              cx="50" 
+              cy="50" 
+              r="45"
+              variants={elapsedTimeVariants}
+              initial="initial"
+              animate={isRunning ? 'start' : hasUpdated ? 'reset' : 'initial'}
+            ></motion.circle>
+            <motion.path
+              id="base-timer-path-remaining"
+              className={styles.timeLeft}
+              d="
+                M 50, 50
+                m -45, 0
+                a 45,45 0 1,0 90,0
+                a 45,45 0 1,0 -90,0
+              "
+              variants={timeLeftVariants}
+              initial="initial"
+              animate={isRunning ? animation : hasUpdated ? 'reset' : 'initial'}
+            ></motion.path>
+          </g>
+        </svg>
+        <div className={styles.label}>
+          <p className={styles.description}>Time until next step:</p>
+          <p className={styles.time}>{getTime()}</p>
+        </div>
       </div>
     </div>
   )
