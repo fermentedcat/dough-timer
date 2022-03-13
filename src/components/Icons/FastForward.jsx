90d983f2af5be7afacbf9882328c2ceccab92078
Maja Thunberg <majaneko@gmail.com>
2022-03-13 17:55:02 +0100

    Add svg icons

---
+++ src/components/Icons/FastForward.jsx
@@ -0,0 +1,14 @@
+const FastForward = () => {
+  return (
+    <svg 
+      focusable="false" 
+      aria-hidden="true" 
+      viewBox="0 0 24 24" 
+      fill="current"
+    >
+      <path d="m4 18 8.5-6L4 6v12zm9-12v12l8.5-6L13 6z"></path>
+    </svg>
+  )
+}
+
+export default FastForward
---
+++ src/components/Icons/Play.jsx
@@ -0,0 +1,14 @@
+const Play = () => {
+  return (
+    <svg 
+      focusable="false" 
+      aria-hidden="true" 
+      viewBox="0 0 24 24"
+      fill="current"
+    >
+      <path d="M8 5v14l11-7z"></path>
+    </svg>
+  )
+}
+
+export default Play
